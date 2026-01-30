58d7d3b570dd1b13dce6015d2985e1a75265a336
DIVYA1956 <komalvk59@gmail.com>
2026-01-31 01:59:42 +0530

    Improve even-odd program with function and input validation

--- even_odd.py
+++ even_odd.py
@@ -1,6 +1,13 @@
-num = int(input("Enter a number: "))
+def check_even_odd(num):
+    if num % 2 == 0:
+        return "Even number"
+    else:
+        return "Odd number"
 
-if num % 2 == 0:
-    print("Even number")
-else:
-    print("Odd number")
+
+try:
+    number = int(input("Enter a number: "))
+    result = check_even_odd(number)
+    print(result)
+except ValueError:
+    print("Please enter a valid integer.")
